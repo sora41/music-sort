a1fdb2778ce615c22b53c6710c2e346de69845f6
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-06-14 14:12:44 +0200

    supresion import inutile

--- src/MyMp3/FileBandMaster.java
+++ src/MyMp3/FileBandMaster.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Iterator;
+
 
 public abstract class FileBandMaster {
 
