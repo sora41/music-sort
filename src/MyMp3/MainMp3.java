ac52c323e9e6e69f73e01eb6423f0153d2b82d71
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-06-25 14:09:19 +0200

    clean- supresion function non utilisée

--- src/MyMp3/MainMp3.java
+++ src/MyMp3/MainMp3.java
@@ -8,7 +8,6 @@ import javax.swing.ButtonGroup;
 public class MainMp3 {
 
 	// TODO 1.4 mettre en place un des test Unitaire
-	// TODO 1.0 netoyer les fonction initile dans la classe main
 
 	/*
 	 * test unitaire stp voir
@@ -22,56 +21,10 @@ public class MainMp3 {
 	private static final String DIRECTORY_SORT = "Music\\sortedMusic";
 	private static final String DIRECTORY_BACK = "Music\\back";
 
-	/*
-	 * public static void TestloadMp3Manuel() {
-	 * 
-	 * TestMp3 tMp3 = new TestMp3(); MusicFileBandMaster bigBrother = new
-	 * MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
-	 * 
-	 * ArrayList<String> listeFichiers =
-	 * bigBrother.getListeFilesMp3(DIRECTORY_IN);
-	 * 
-	 * System.out.println("----------------------------------------");
-	 * System.out.println("n0 " + listeFichiers.get(0));
-	 * tMp3.loadMp3Mannuel(DIRECTORY_IN + File.separator +
-	 * listeFichiers.get(0));
-	 * System.out.println("----------------------------------------");
-	 * System.out.println("n1 " + listeFichiers.get(1));
-	 * tMp3.loadMp3Mannuel(DIRECTORY_IN + File.separator +
-	 * listeFichiers.get(1));
-	 * System.out.println("----------------------------------------");
-	 * System.out.println("n2 " + listeFichiers.get(2));
-	 * tMp3.loadMp3Mannuel(DIRECTORY_IN + File.separator +
-	 * listeFichiers.get(2));
-	 * System.out.println("----------------------------------------");
-	 * System.out.println("n7 " + listeFichiers.get(7));
-	 * tMp3.loadMp3Mannuel(DIRECTORY_IN + File.separator +
-	 * listeFichiers.get(7));
-	 * System.out.println("----------------------------------------");
-	 * System.out.println("n8 " + listeFichiers.get(8));
-	 * tMp3.loadMp3Mannuel(DIRECTORY_IN + File.separator +
-	 * listeFichiers.get(8));
-	 * 
-	 * }
-	 */
-
 	public static void MainRunSort() {
 		MusicFileBandMaster bigBrother = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
 		bigBrother.runSortFile();
 	}
-
-	/*
-	 * public static void testFileMethodeList() {
-	 * 
-	 * File f = new File(DIRECTORY_IN); String[] tabName = f.list();
-	 * System.out.println("testFileMetodeList"); afficheTab(tabName);
-	 * 
-	 * }
-	 * 
-	 * public static void afficheTab(String[] tab) { if (tab != null) if
-	 * (tab.length > 0) for (int i = 0; i < tab.length; i++) {
-	 * System.out.println(tab[i]); } }
-	 */
 
 	public static void initDirectory() {
 		// System.out.println("initDirectori");
@@ -92,10 +45,7 @@ public class MainMp3 {
 		System.out.println("lancement initalisation");
 		initDirectory();
 		System.out.println("lancement tri");
-		MainRunSort();
-
-		// TestloadMp3Manuel();
-		// testFileMethodeList();
+		MainRunSort();	
 
 	}
 
