3ee796e591e341c2573c8309679323047bd84527
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-07-05 17:51:14 +0200

    feature-add-class MusicDto

---
+++ src/datatransfert/MusicDto.java
@@ -0,0 +1,45 @@
+package datatransfert;
+
+public class MusicDto {
+
+	private String fileName;
+
+	private String author;
+
+	private String album;
+
+	private String pathFile;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getPathFile() {
+		return pathFile;
+	}
+
+	public void setPathFile(String pathFile) {
+		this.pathFile = pathFile;
+	}
+
+}
