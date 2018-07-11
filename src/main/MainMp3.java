f0cc43e602bd1f346604c3d5eb864ec81fbc7cb8
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-07-11 10:35:10 +0200

    refactor--MainClass

--- src/main/MainMp3.java
+++ src/main/MainMp3.java
@@ -4,35 +4,27 @@ import bandMaster.FileBandMaster;
 import bandMaster.MusicFileBandMaster;
 
 public class MainMp3 {
-	
+
 	private static final String DIRECTORY_IN = "Music\\inMusic";
 	private static final String DIRECTORY_OUT = "Music\\outMusic";
 	private static final String DIRECTORY_SORT = "Music\\sortedMusic";
 	private static final String DIRECTORY_BACK = "Music\\back";
 
-	public static void MainRunSort() {
-		MusicFileBandMaster bigBrother = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
-		bigBrother.runSortFile();
-	}
+	private static MusicFileBandMaster musicSorter;
 
-	public static void initDirectory() {
-		MusicFileBandMaster bigBrother = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
-		bigBrother.initDirectorieIn(DIRECTORY_BACK);
-	}
-
-	public static void resetDirectory() {
-		FileBandMaster bigBrother = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
-		bigBrother.resetDirectories();
+	public static void initApplication() {
+		musicSorter = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
 	}
 
 	public static void main(String[] args) {
 
+		initApplication();
 		System.out.println("lancement reset");
-		resetDirectory();
+		musicSorter.resetDirectories();
 		System.out.println("lancement initalisation");
-		initDirectory();
+		musicSorter.initDirectorieIn(DIRECTORY_BACK);
 		System.out.println("lancement tri");
-		MainRunSort();	
+		musicSorter.runSortFile();
 
 	}
 
