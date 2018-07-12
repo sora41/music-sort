6e1351c30302fa47d2a90db2d841e460c3117cb6
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-07-12 17:57:54 +0200

    refactor --  deplacement et ajout de function

--- src/main/MainMp3.java
+++ src/main/MainMp3.java
@@ -1,6 +1,5 @@
 package main;
 
-import bandMaster.FileBandMaster;
 import bandMaster.MusicFileBandMaster;
 
 public class MainMp3 {
@@ -11,18 +10,30 @@ public class MainMp3 {
 	private static final String DIRECTORY_BACK = "Music\\back";
 
 	private static MusicFileBandMaster musicSorter;
+	private static TestCode test;
 
 	public static void initApplication() {
 		musicSorter = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
+		test = new TestCode();
 	}
 
-	public static void main(String[] args) {
-		initApplication();
+	public static void runTest() {
+		
+		test.runTest();
+	}
+
+	public static void runProcces() {
 		System.out.println("lancement reset");
 		musicSorter.resetDirectories();
 		System.out.println("lancement initalisation");
 		musicSorter.initDirectorieIn(DIRECTORY_BACK);
 		System.out.println("lancement tri");
 		musicSorter.runSortFile();
+	}
+
+	public static void main(String[] args) {
+		initApplication();
+		runProcces();
+		//runTest();
 	}
 }
