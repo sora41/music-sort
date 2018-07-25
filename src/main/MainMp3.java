dad100f12a200ba20333e707dfabfeb094d01d65
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-07-25 17:08:20 +0200

    feature --   addlog on MainMp3 to see how to use

--- src/main/MainMp3.java
+++ src/main/MainMp3.java
@@ -1,5 +1,12 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 import bandMaster.MusicFileBandMaster;
 
 public class MainMp3 {
@@ -12,28 +19,47 @@ public class MainMp3 {
 	private static MusicFileBandMaster musicSorter;
 	private static TestCode test;
 
+	private static Logger loggerMp3 = Logger.getLogger(MainMp3.class.getName());
+
+	private static void initLog() {
+
+		try {
+			LogManager.getLogManager().readConfiguration(new FileInputStream("mp3logging.properties"));
+		} catch (SecurityException | IOException e1) {
+			e1.printStackTrace();
+		}
+
+		loggerMp3.setLevel(Level.FINE);
+		//loggerMp3.addHandler(new ConsoleHandler());
+	}
+
 	public static void initApplication() {
 		musicSorter = new MusicFileBandMaster(DIRECTORY_IN, DIRECTORY_OUT, DIRECTORY_SORT);
 		test = new TestCode();
+		initLog();
 	}
 
 	public static void runTest() {
-		
+
 		test.runTest();
 	}
 
 	public static void runProcces() {
-		System.out.println("lancement reset");
+		//System.out.println("lancement reset");
+		loggerMp3.log(Level.INFO, "lancement reset");
 		musicSorter.resetDirectories();
-		System.out.println("lancement initalisation");
+		//System.out.println("lancement initalisation");
+		loggerMp3.log(Level.INFO, "lancement initalisation");
 		musicSorter.initDirectorieIn(DIRECTORY_BACK);
-		System.out.println("lancement tri");
+		//System.out.println("lancement tri");
+		loggerMp3.log(Level.INFO, "lancement tri");
 		musicSorter.runSortFile();
 	}
 
 	public static void main(String[] args) {
+
 		initApplication();
 		runProcces();
-		//runTest();
+		// runTest();
 	}
 }
