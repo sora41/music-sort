e0e78990b5338a2ae382124d6757dd94fababec4
Florent Galloni <florent.galloni-d-istria@capgemini.com>
2018-08-17 17:06:16 +0200

    refactor --mise en forme

--- src/main/java/repository/IRepositoryFile.java
+++ src/main/java/repository/IRepositoryFile.java
@@ -11,9 +11,9 @@ public interface IRepositoryFile {
 
 	public abstract void recursiveDelete(String pathFileName) throws  IOException;
 
-	public abstract void move(String OrginalePathName, String FinalPahtName) throws IOException;
+	public abstract void move(String orginalePathName, String finalPahtName) throws IOException;
 
-	public abstract void copy(String OrginalePathName, String FinalPahtName) throws IOException;
+	public abstract void copy(String orginalePathName, String finalPahtName) throws IOException;
 	
 	public abstract boolean validateDirectory(File dir); 
 	
