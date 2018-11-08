70715944c2c3293d399a8fc237b712e5d56b5343
florent galloni d'istria <florent.galloni-d-istria@capgemini.com>
2018-11-08 17:57:42 +0100

    refactor -- ajustement du code pour la regle de compilance jdk 1.8 fix -- ameilloration prise en charge des erreur lie au tag non renseigné sur les mp3 dans le RepositoryMusicFileMp3JAudiotagger.java

--- src/main/java/repository/music/RepositoryMusicFileMp3JAudiotagger.java
+++ src/main/java/repository/music/RepositoryMusicFileMp3JAudiotagger.java
@@ -1,10 +1,13 @@
 package repository.music;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.TagField;
 import org.jaudiotagger.tag.TagNotFoundException;
 import org.jaudiotagger.tag.id3.ID3v24Frames;
 
@@ -12,8 +15,8 @@ import datatransfert.MusicDto;
 import repository.IRepositoryMusicFile;
 
 public class RepositoryMusicFileMp3JAudiotagger implements IRepositoryMusicFile {
-		
-	@Override
+
+
 	public MusicDto getDataToMusicFile(String pathFileName) throws Exception {
 		MusicDto result = null;
 		MP3File mp3File = null;
@@ -34,17 +37,32 @@ public class RepositoryMusicFileMp3JAudiotagger implements IRepositoryMusicFile 
 		return result;
 	}
 
-	@Override
 	public boolean saveDataToMusicFile(MusicDto data) throws Exception {
 		Exception e = new Exception("fonction non implementer");
 		throw e;
 	}
 
-	private MusicDto getID3DataV1(MP3File mp3File) {
+
+	private void checkTagfield(List<TagField> tagFieldList,String tag) throws TagException
+	{
+
+		if (tagFieldList.isEmpty()== true)
+		{
+			TagException tagException = new TagException(" "+tag+" :Tag is empthy ");
+			throw tagException;
+		}
+	}
+	private MusicDto getID3DataV1(MP3File mp3File) throws TagException {
 		MusicDto songDto = new MusicDto();
-		songDto.setAlbum(mp3File.getID3v1Tag().getAlbum().get(0).toString());
+		List<TagField> albums = null;
+		List<TagField> artists = null;
+		albums = mp3File.getID3v1Tag().getAlbum();
+		artists = mp3File.getID3v1Tag().getArtist();
+		this.checkTagfield(artists,"artist");
+		this.checkTagfield(albums,"album");
+		songDto.setAlbum(albums.get(0).toString());
 		songDto.setFileName(mp3File.getFile().getName());
-		songDto.setAuthor(mp3File.getID3v1Tag().getArtist().get(0).toString());
+		songDto.setAuthor(artists.get(0).toString());
 		songDto.setPathFile(mp3File.getFile().getPath());
 		songDto.setTitleSong(mp3File.getID3v1Tag().getFirstTitle());
 		songDto.setYears(mp3File.getID3v1Tag().getFirstYear());
