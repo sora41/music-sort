28d708be2d57adb471820377dae650c56edf6ce8
sora41 <sclad41@gmail.com>
2019-06-10 10:54:57 +0200

    netoyage class constant/MusicExtention.java

--- src/main/java/constant/MusicExtention.java
+++ src/main/java/constant/MusicExtention.java
@@ -1,25 +1,19 @@
 package constant;
 
-public enum MusicExtention { 
-	MP3("mp3","repository.music.RepositoryMusicFileMp3JAudiotagger"), 
-	WMA("wma","repository.music.RepositoryMusicFileWmaJAudiotagger");
-	//,M4A("m4a"),MP4("mp4"),FLAC("flac");
+public enum MusicExtention {
+	MP3("mp3"), WMA("wma");
 
 	private String value = "";
-	private String repoClass = "";
 
-	MusicExtention(String value,String repoClass) {
+	MusicExtention(String value) {
 		this.value = value;
-		this.repoClass= repoClass;
+
 	}
 
 	public String getValue() {
 		return this.value;
 	}
-	
-	public String getRepoClass() {
-		return this.repoClass;
-	}
+
 	@Override
 	public String toString() {
 		return this.value;
