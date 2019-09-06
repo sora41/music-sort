5002d26365937daec13c1c5718afdd3db4e8e8b1
florent galloni d'istria <florent.galloni-d-istria@capgemini.com>
2019-09-06 17:15:20 +0200

    feat -- maj IHM

--- src/main/java/ihm/MusicPanel.java
+++ src/main/java/ihm/MusicPanel.java
@@ -1,14 +1,11 @@
 package ihm;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -17,18 +14,22 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import main.ControlerSortMusic;
-import main.MainThreadMusic;
 import observer.Observateur;
 
 public class MusicPanel extends JPanel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	// private JPanel loadFilePanel = new JPanel();
 	private JButton startSortButton = new JButton("Start Sort");
-	private JFileChooser musicFileChooser = new JFileChooser();
+	// private JFileChooser musicFileChooser = new JFileChooser();
 	// ligne/colone
-	private GridLayout gridLayout = new GridLayout(4,0);
-	private GridLayout Line1Layout = new GridLayout(0,1);
-	private GridLayout Line2Layout = new GridLayout(0,1);
+	private GridLayout line4Layout = new GridLayout(4, 0);
+	private GridLayout column4Layout = new GridLayout(0, 4);
+	private GridLayout line2Layout = new GridLayout(2, 0);
+
 	private JLabel dirOutLabel = new JLabel();
 	private JLabel dirOutValueLabel = new JLabel();
 	private JLabel dirInValueLabel = new JLabel();
@@ -36,16 +37,16 @@ public class MusicPanel extends JPanel {
 	private JLabel dirBackLabel = new JLabel();
 	private JLabel dirBackValueLabel = new JLabel();
 	private JProgressBar sortedBar = new JProgressBar();
-	private JLabel chargementLabel = new JLabel();
-	//private BorderLayout mainBorder = new BorderLayout();
+	private JLabel loadingSortLabel = new JLabel();
+	private JProgressBar resetBar = new JProgressBar();
+	private JLabel loadingResetLabel = new JLabel();
+
 	private JPanel linePanel1 = new JPanel();
 	private JPanel linePanel2 = new JPanel();
 	private JPanel linePanel3 = new JPanel();
 	private JPanel linePanel4 = new JPanel();
-	//private JPanel panelNord = new JPanel();
-	//private JPanel panelSouth = new JPanel();
-	//private JPanel panelEast = new JPanel();
-	//private JPanel panelWest = new JPanel();
+	private JPanel linePanel5 = new JPanel();
+
 	/**
 	 * the loger from log4j
 	 */
@@ -54,19 +55,33 @@ public class MusicPanel extends JPanel {
 	private ControlerSortMusic musicControl;
 	private Thread musicSorterThread;
 
+	private void initDirLabel() {
+
+		dirBackLabel.setText("repertoire Back :");
+		dirInLabel.setText("repertoire In :");
+		dirOutLabel.setText("repertoire Out :");
+
+		dirOutValueLabel.setText(musicControl.getDirectoryOut());
+		dirInValueLabel.setText(musicControl.getDirectoryIn());
+		dirBackValueLabel.setText(musicControl.getDirectoryBack());
+	}
+
 	public MusicPanel() {
 
-		this.setLayout(gridLayout);
+		this.setLayout(line4Layout);
 
-		
 		startSortButton.addActionListener(new ButtonLoadListener());
-		
+
 		this.add(linePanel1);
 		this.add(linePanel2);
 		this.add(linePanel3);
 		this.add(linePanel4);
-		
-		
+
+		linePanel1.setLayout(column4Layout);
+		linePanel2.setLayout(column4Layout);
+		linePanel3.setLayout(column4Layout);
+		linePanel4.setLayout(line2Layout);
+
 		linePanel1.add(dirBackLabel);
 		linePanel1.add(dirBackValueLabel);
 		linePanel2.add(dirInLabel);
@@ -75,45 +90,37 @@ public class MusicPanel extends JPanel {
 		linePanel3.add(dirOutValueLabel);
 		linePanel4.add(startSortButton);
 		linePanel4.add(sortedBar);
-		// this.add(musicFileChooser);
-		chargementLabel.setText("0");
-		musicControl = new ControlerSortMusic();
-		
-		dirBackLabel.setText("repertoire Back :");
-		dirInLabel.setText("repertoire In :");
-		dirOutLabel.setText("repertoire Out :");
-		
-		dirOutValueLabel.setText(musicControl.getDIRECTORY_OUT());
-		dirInValueLabel.setText(musicControl.getDIRECTORY_IN());
-		dirBackValueLabel.setText(musicControl.getDIRECTORY_BACK());
-		
-		try {
+		linePanel3.add(loadingSortLabel);
+		// linePanel1.add(musicFileChooser);
 
+		loadingSortLabel.setText("0");
+		musicControl = new ControlerSortMusic();
+		initDirLabel();
+
+		try {
 			musicControl.initApplication();
 			musicControl.getMusicSorter().addObservateur(new Observateur() {
 
 				public void update(int enCours, int fin) {
 
-					chargementLabel.setText(String.valueOf(enCours));
+					loadingSortLabel.setText(String.valueOf(enCours));
 					sortedBar.setMaximum(fin);
 					sortedBar.setValue(enCours);
-					
+
 					if (enCours == fin) {
 						startSortButton.setEnabled(true);
 					}
 				}
 			});
 		} catch (Exception e) {
-
 			LOGGER4J.fatal("l'application c'est arrete de maniere inatendu ", e.getClass(), e.getMessage(),
 					e.getStackTrace());
 			LOGGER4J.fatal("message :" + e.getMessage());
 			LOGGER4J.fatal("Eclass :" + e.getClass().getName());
-
 		}
 	}
 
-	public void runProccesWithReset(boolean reset) throws IOException {
+	private void runProccesWithReset(boolean reset) throws IOException {
 		if (reset) {
 			musicControl.resetDirectory();
 			musicControl.initDirectory();
@@ -125,7 +132,7 @@ public class MusicPanel extends JPanel {
 		public void actionPerformed(ActionEvent ae) {
 			JButton loadButton;
 			LOGGER4J.info("Debut clic sur load");
-			chargementLabel.setText("0");
+			loadingSortLabel.setText("0");
 			sortedBar.setValue(0);
 			musicSorterThread = new Thread(new MusicThread());
 			musicSorterThread.start();
@@ -135,7 +142,16 @@ public class MusicPanel extends JPanel {
 		}
 	}
 
-	class MusicThread implements Runnable {
+	private class MusicThread implements Runnable {
+
+		private void runProccesWithReset(boolean reset) throws IOException {
+			if (reset) {
+				musicControl.resetDirectory();
+				musicControl.initDirectory();
+			}
+			musicControl.launchSort();
+		}
+
 		public void run() {
 			try {
 				runProccesWithReset(true);
