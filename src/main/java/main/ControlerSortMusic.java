09c0244875d54f0e9ebf28eb0fe3c7c9821c9fb2
sora41 <sclad41@gmail.com>
2020-02-18 00:02:53 +0100

    FIX -- name package

--- src/main/java/main/ControlerSortMusic.java
+++ src/main/java/main/ControlerSortMusic.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import bandMaster.MusicFileBandMaster;
+import bandmaster.MusicFileBandMaster;
 
 public class ControlerSortMusic {
 
