22c33b173e6ed55c1ee28d5884ce2222560a7d63
Sora41 <sclad41@gmail.com>
2020-12-10 21:49:20 +0100

    CONF--maj git ignore

--- src/main/java/ihm/MusicFrame.java
+++ src/main/java/ihm/MusicFrame.java
@@ -8,13 +8,14 @@ public class MusicFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static final String MUSIC_FRAME_NAME ="MusicSorter";
 	
 	//private MusicPanel2 musicPanel2 = new MusicPanel2();
 	private MusicPanel musicPanel = new MusicPanel();
 
 	public MusicFrame()
 		  {  
-		     super("MusicSorter");
+		     super(MUSIC_FRAME_NAME);
 		     this.setSize(500,500); 
 		     this.setLocationRelativeTo(null);
 		     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
